8263539ea27756389bed3e78138c581d949c5f05
rnc-personal <richard.challenger@protonmail.com>
2022-11-07 09:35:20 +0000

    setup test button for generating a card on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const test = document.querySelector('.test')
+
+
+console.log(test)
+
+
+test.addEventListener('click', function() {
+  
+    const card = document.createElement('div')
+    card.classList.add('card-test');
+    main.appendChild(card)
+
+})
