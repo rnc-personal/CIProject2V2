a71d02f45d77aa57ef7716797cc57de8c5c688f1
rnc-personal <richard.challenger@protonmail.com>
2022-11-07 11:13:03 +0000

    testing out combining search term with api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,12 @@
+//API 
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=58b0219670380f0425dd7bec4738e064&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=58b0219670380f0425dd7bec4738e064&query="'                 //note the space in readme
+
 // defining some generic elements to use
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-const test = document.querySelector('.test')
-
-//Test for Event handling/Generating element
-// test.addEventListener('click', function() {
-  
-//     const card = document.createElement('div')
-//     card.classList.add('film-card');
-//     main.appendChild(card)
-
-// })
 
 form.addEventListener('submit',function(e){
     //Using preventDefault as we want to submit the search terms but not have the page reload
@@ -21,7 +16,7 @@ form.addEventListener('submit',function(e){
     console.log(searchInput)
 
     if(searchInput) {
-        main.innerHTML = `<h1>${searchInput}</h1>`
+        main.innerHTML = `<h1>${SEARCH_API + searchInput}</h1>`
     } else if (searchInput == '') {
         window.location.reload()
     }
