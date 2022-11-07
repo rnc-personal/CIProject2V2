3e9943c8a230e14eb56f098c3c1b0c2cb66b26d5
rnc-personal <richard.challenger@protonmail.com>
2022-11-07 16:14:18 +0000

    testing out forEach on results(*Not Working)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //API 
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=58b0219670380f0425dd7bec4738e064&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=58b0219670380f0425dd7bec4738e064&query="'                 //note the space in readme
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=58b0219670380f0425dd7bec4738e064&query="' //note the space in readme
 
 // defining some generic elements to use
 const main = document.getElementById('main')
@@ -25,11 +25,8 @@ async function findFilms(url) {
  * Primary Function of the app, loops through the return results from findMovies() and creates a card for each results, with the data
  */
 function buildResults(films) {
-    
-    for (let film of films) {
-        // let film = [title, poster_path, vote_average, overview]                                     // BUG: Storing this as an array doesnt work
+    films.forEach((film) => {
 
-        //Create cards for each result
         const filmCard = document.createElement('div')
         filmCard.classList.add('film-card')
 
@@ -37,12 +34,16 @@ function buildResults(films) {
         filmCard.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         `
-
         main.appendChild(filmCard)
-    }
+    })
+    // let film = [title, poster_path, vote_average, overview]                                     // BUG: Storing this as an array doesnt work
+
+    //Create cards for each result
+
+
 }
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit', function (e) {
     //Using preventDefault as we want to submit the search terms but not have the page reload
     e.preventDefault()
     console.log('submitted')
@@ -50,11 +51,11 @@ form.addEventListener('submit',function(e){
     searchInput = search.value
     console.log(searchInput)
 
-    if(searchInput) {
+    if (searchInput) {
         findFilms(SEARCH_API + searchInput)
-        search.value = ''                                           // Clearing the search value after a submissions
+        search.value = '' // Clearing the search value after a submissions
         // main.innerHTML = `<h1>${SEARCH_API + searchInput}</h1>`
-    } else  {
+    } else {
         window.location.reload()
     }
 })
