8ada7d5590771287671f1bd5c2f59096e1624b8d
rnc-personal <richard.challenger@protonmail.com>
2022-11-08 15:07:39 +0000

    removing slide from main js file  into new file

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,49 +15,7 @@ const search = document.getElementById('search')
 const trendingSection = document.querySelector('.trending')
 const newSection = document.querySelector('.new-releases')
 const upcomingSection = document.querySelector('.upcoming')
-const slides = document.querySelectorAll('.slide')
 
-//******
-//Home Slides
-//******
-
-//Looping through slide elements to form an array and set their position
-slides.forEach((slide, idx) => {
-    slide.style.transform = `translateX(${idx * 100}%)`;
-  });
-
-//Slider Animation
-let currentSlide = 0;
-let maxSlides = slides.length - 1
-
-const prevSlide = document.querySelector('.btn-prev')
-const nextSlide = document.querySelector('.btn-next')
-
-// Next Control - On clicking the controls, update the slide: 
-nextSlide.addEventListener('click', function() {
-    if (currentSlide === maxSlides) {
-        currentSlide = 0
-    } else {
-        currentSlide++
-    }
-
-    slides.forEach((slide, idx) => {
-        slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`
-    })
-})
-
-// Prev Control - On clicking the controls, update the slide if its not the first one: 
-prevSlide.addEventListener('click', function() {
-    if (currentSlide === 0) {
-        currentSlide = 0
-    } else {
-        currentSlide--
-    }
-
-    slides.forEach((slide, idx) => {
-        slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`
-    })
-})
 
 //Make an initall call to results function so the page is not blank - returns most popular results
 console.log(window.location.pathname)
