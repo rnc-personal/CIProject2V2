d49bcd8a4894dd68f18827c241410cdbb4203095
rnc-personal <richard.challenger@protonmail.com>
2022-11-10 10:14:32 +0000

    install jshint, fixed formatting in js (semicolons)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
 //API 
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=58b0219670380f0425dd7bec4738e064&page=1'
-const TRENDING_URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=58b0219670380f0425dd7bec4738e064&page=1'
-const LATEST_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=58b0219670380f0425dd7bec4738e064'
-const UPCOMING_URL = 'https://api.themoviedb.org/3/movie/upcoming?api_key=58b0219670380f0425dd7bec4738e064&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=58b0219670380f0425dd7bec4738e064&query="' //note the space in readme
-const NOW_PLAYING = 'https://api.themoviedb.org/3/movie/now_playing?api_key=58b0219670380f0425dd7bec4738e064'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=58b0219670380f0425dd7bec4738e064&page=1';
+const TRENDING_URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=58b0219670380f0425dd7bec4738e064&page=1';
+const LATEST_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=58b0219670380f0425dd7bec4738e064';
+const UPCOMING_URL = 'https://api.themoviedb.org/3/movie/upcoming?api_key=58b0219670380f0425dd7bec4738e064&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=58b0219670380f0425dd7bec4738e064&query="'; //note the space in readme
+const NOW_PLAYING = 'https://api.themoviedb.org/3/movie/now_playing?api_key=58b0219670380f0425dd7bec4738e064';
 
 // defining some generic elements to use
-const main = document.getElementById('main')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const trendingSection = document.querySelector('.trending')
-const cardArray = document.getElementsByClassName('film-card')
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const trendingSection = document.querySelector('.trending');
+const cardArray = document.getElementsByClassName('film-card');
 
 //Make an initial call to results function so the page is not blank - returns most popular results
 //Results depend on page
 // '/CIProject2V2/index.html'
 
 if (window.location.pathname == '/CIProject2V2/index.html') {
-    document.body.style.cssText = "height:1300px"
+    document.body.style.cssText = "height:1300px";
 
     //******
     //Home Hero Slides
     //******
-    const slides = document.querySelectorAll('.slide')
+    const slides = document.querySelectorAll('.slide');
 
     //Looping through slide elements to form an array and set their position
     slides.forEach((slide, idx) => {
@@ -33,46 +33,46 @@ if (window.location.pathname == '/CIProject2V2/index.html') {
 
     //Slider Animation
     let currentSlide = 0;
-    let maxSlides = slides.length - 1
+    let maxSlides = slides.length - 1;
 
-    const prevSlide = document.querySelector('.btn-prev')
-    const nextSlide = document.querySelector('.btn-next')
+    const prevSlide = document.querySelector('.btn-prev');
+    const nextSlide = document.querySelector('.btn-next');
 
     // Next Control - On clicking the controls, update the slide: 
     nextSlide.addEventListener('click', function () {
         if (currentSlide === maxSlides) {
-            currentSlide = 0
+            currentSlide = 0;
         } else {
-            currentSlide++
+            currentSlide++;
         }
 
         slides.forEach((slide, idx) => {
-            slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`
-        })
-    })
+            slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`;
+        });
+    });
 
     // Prev Control - On clicking the controls, update the slide if its not the first one: 
     prevSlide.addEventListener('click', function () {
         if (currentSlide === 0) {
-            currentSlide = 0
+            currentSlide = 0;
         } else {
-            currentSlide--
+            currentSlide--;
         }
 
         slides.forEach((slide, idx) => {
-            slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`
-        })
-    })
+            slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`;
+        });
+    });
 
     //Called here so the below event listeners can apply to the loaded cards
-    findFilms(TRENDING_URL)
+    findFilms(TRENDING_URL);
 
 } else if (window.location.pathname === '/CIProject2V2/trending.html') {
-    findFilms(TRENDING_URL)
+    findFilms(TRENDING_URL);
 } else if (window.location.pathname === '/CIProject2V2/current.html') {
-    findFilms(NOW_PLAYING)
+    findFilms(NOW_PLAYING);
 } else {
-    findFilms(UPCOMING_URL)
+    findFilms(UPCOMING_URL);
 }
 
 //Using async to only display results when we have the data back
@@ -83,10 +83,10 @@ if (window.location.pathname == '/CIProject2V2/index.html') {
  * the url parameter is passed the full SEARCH_API + searchInput (the users request) to make a complete fetch request.
  * The results are then passed into the buildResults functions (films) parameter with the response, which is a JSON object. */
 async function findFilms(url) {
-    const res = await fetch(url)
-    const data = await res.json()
+    const res = await fetch(url);
+    const data = await res.json();
 
-    buildResults(data.results)
+    buildResults(data.results);
 }
 
 /**
@@ -96,7 +96,7 @@ function buildResults(films) {
 
     if (window.location.pathname === '/CIProject2V2/search.html') {
         //clears the default results when a search request is submitted and this function is called.
-        main.innerHTML = ''
+        main.innerHTML = '';
     }
 
     // Destructuring the response (see notes/references)
@@ -106,19 +106,14 @@ function buildResults(films) {
             poster_path,
             vote_average,
             overview
-        } = film
+        } = film;
 
         //The data and the response are both objects so the properties we want to use are defined here (using their names in the object/API docs)
         //A 'film' is made up of those specific object properties and can be used to output the data.
         //The loop returns the next set of data and will apply the specific data to that card below:
 
-        // setTimeout(() => {
-        //     const homeCards = document.querySelector('.trending')
-        //     console.log(homeCards.childNodes.length, 'film cards')
-        // }, 4000);
-
-        const filmCard = document.createElement('div')
-        filmCard.classList.add('film-card')
+        const filmCard = document.createElement('div');
+        filmCard.classList.add('film-card');
 
         //Filling Card with the data that is returned
         filmCard.innerHTML = `
@@ -132,19 +127,19 @@ function buildResults(films) {
                 ${overview}
             </div>
         </div>
-        `
+        `;
         //Create cards for each result. Additional closing div to close the one created in line 42
         //Conditionally appends different cards depending on page.
         if (window.location.pathname === '/CIProject2V2/index.html') {
-            trendingSection.appendChild(filmCard)
+            trendingSection.appendChild(filmCard);
         } else if (window.location.pathname === '/CIProject2V2/trending.html') {
-            main.appendChild(filmCard)
+            main.appendChild(filmCard);
         } else if (window.location.pathname === '/CIProject2V2/current.html') {
-            main.appendChild(filmCard)
+            main.appendChild(filmCard);
         } else if (window.location.pathname === '/CIProject2V2/search.html') {
-            main.appendChild(filmCard)
+            main.appendChild(filmCard);
         }
-    })
+    });
 }
 
 /**This applies a class dynamically based on the score value returned for the API
@@ -153,41 +148,41 @@ function buildResults(films) {
 
 function applyScoreClass(vote) {
     if (vote >= 8) {
-        return 'green'
+        return 'green';
     } else if (vote >= 5) {
-        return 'orange'
+        return 'orange';
     } else {
-        return 'red'
+        return 'red';
     }
 }
 
 
 form.addEventListener('submit', function (e) {
     //Using preventDefault as we want to submit the search terms but not have the page reload
-    e.preventDefault()
+    e.preventDefault();
 
-    const searchInput = search.value
+    const searchInput = search.value;
 
     if (searchInput) {
-        findFilms(SEARCH_API + searchInput)
+        findFilms(SEARCH_API + searchInput);
         // Clearing the search value after a submissions
-        search.value = ''
+        search.value = '';
     } else {
-        window.location.reload()
+        window.location.reload();
     }
-})
+});
 
 function miniNavToggle() {
-    const miniMenu = document.getElementById('footer')
-    miniMenu.classList.toggle('mob-open')
+    const miniMenu = document.getElementById('footer');
+    miniMenu.classList.toggle('mob-open');
 
 }
 
 if (window.location.pathname === '/CIProject2V2/index.html') {
     setTimeout(() => {
 
-        cardArrayNew = Array.from(cardArray)
-        const homeCards = document.querySelector('.trending')
+        cardArrayNew = Array.from(cardArray);
+        const homeCards = document.querySelector('.trending');
         
         //******
         //Home Hero Cards
@@ -195,44 +190,44 @@ if (window.location.pathname === '/CIProject2V2/index.html') {
 
         //Looping through slide elements to form an array and set their position
         for (let i = 0; i < cardArrayNew.length; i++) {
-            let card = cardArrayNew[i]
+            let card = cardArrayNew[i];
             
             
         }
 
         let currentCard = 0;
-        let maxCards = cardArrayNew.length / 2
-        const prevCard = document.querySelector('.promo-card-prev')
-        const nextCard = document.querySelector('.promo-card-next')
+        let maxCards = cardArrayNew.length / 2;
+        const prevCard = document.querySelector('.promo-card-prev');
+        const nextCard = document.querySelector('.promo-card-next');
 
         nextCard.addEventListener('click', function () {
             if (currentCard === maxCards) {
-                currentCard = 0
+                currentCard = 0;
                 
             } else {
-                currentCard++
+                currentCard++;
                 
             }
 
             cardArrayNew.forEach((card, idx) => {
                 
-                trendingSection.style.transform = `translateX(-${2.4 * (idx - currentCard)}%)`
-            })
-        })
+                trendingSection.style.transform = `translateX(-${2.4 * (idx - currentCard)}%)`;
+            });
+        });
 
         prevCard.addEventListener('click', function () {
             if (currentCard === 0) {
-                currentCard = 0
+                currentCard = 0;
                 
             } else {
-                currentCard--
+                currentCard--;
                 
             }
 
             cardArrayNew.forEach((card, idx) => {
         
-                trendingSection.style.transform = `translateX(${2 / (idx - currentCard)}%)`
-            })
+                trendingSection.style.transform = `translateX(${2 / (idx - currentCard)}%)`;
+            });
 
         });
     }, 4000);
